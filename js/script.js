655d0d6681c8e5095fff3dd64f0b6eab4c0844a3
ndonye mark <ndonyemark@gmail.com>
2020-02-17 05:10:09 +0300

    add the control flow and set the date

--- js/script.js
+++ js/script.js
@@ -4,4 +4,37 @@ function fn(){
     var date = parseInt(document.getElementById("day").value);
     var month = parseInt(document.getElementById("month").value) - 1;
     var year = parseInt(document.getElementById("year").value);
+
+    if (day < 0 || day > 31) {
+        document.getElementsByClassName("dayerror").innerHtml = "Enter valid date";
+        return false;
+    } else if (date == "") {
+        document.getElementsByClassName("dayerror").innerHtml = "This field cannot be empty";
+        return false;
+    } else {
+        d.setDate(date);
+    }
+
+    if (month < 0 || day > 12) {
+        document.getElementsByClassName("montherror").innerHtml = "Enter valid month";
+        return false;
+    } else if (month == "") {
+        document.getElementsByClassName("montherror").innerHtml = "This field cannot be empty";
+        return false;
+    } else {
+        d.setMonth(month);
+    }
+
+    if (month < 0) {
+        document.getElementsByClassName("yearerror").innerHtml = "Enter valid year";
+        return false;
+    } else if (month == "") {
+        document.getElementsByClassName("yearerror").innerHtml = "This field cannot be empty";
+        return false;
+    } else {
+        d.setYear(year);
+    }
+
+    var day = d.getDay();
+
 }
