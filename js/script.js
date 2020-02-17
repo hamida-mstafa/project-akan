696ab110243d591b463c4e19e855a7ea1877a872
ndonye mark <ndonyemark@gmail.com>
2020-02-17 05:08:40 +0300

    add the date variables

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 function fn(){
-    
+    var d = new Date();
+
+    var date = parseInt(document.getElementById("day").value);
+    var month = parseInt(document.getElementById("month").value) - 1;
+    var year = parseInt(document.getElementById("year").value);
 }
